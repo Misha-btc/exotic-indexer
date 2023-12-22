6bd9a369e8e5b63e47c3812275f9facf6502c163
Misha Sychev <91029010+Misha-btc@users.noreply.github.com>
2023-12-22 06:11:36 +0200

    Update sat.go

--- exotic/sat.go
+++ exotic/sat.go
@@ -19,20 +19,12 @@ var HitmanRanges = SatingRangesToOrdinalsRanges(hitmanSatingRanges)
 var JpegRanges = SatingRangesToOrdinalsRanges(jpegSatingRanges)
 
 type Sat struct {
-    ID             int64    // Уникальный идентификатор сатоши
-    TransactionIDs []string // Список идентификаторов транзакций
+    ID              int64 // Уникальный идентификатор сатоши
+    TransactionCount int  // Количество транзакций, в которых участвовал сатоши
 }
 
 func (s *Sat) Epoch() Epoch {
     return EpochFromSat(s.ID)
-}
-
-func (s *Sat) Cycle() int64 {
-    return s.Height() / CycleInterval
-}
-
-func (s *Sat) Period() int64 {
-    return s.Height() / DificultyAdjustmentInterval
 }
 
 func (s *Sat) EpochPosition() int64 {
@@ -52,122 +44,16 @@ func (s *Sat) IsFirstSatInBlock() bool {
     return int64(s.ID)%sub == 0
 }
 
-func (s Sat) GetRodarmorRarity() Satribute {
-	isFirstSatInBlock := s.IsFirstSatInBlock()
-	h := s.Height()
-
-	if s == 0 {
-		return Mythic
-	}
-
-	if isFirstSatInBlock && h%CycleInterval == 0 {
-		return Legendary
-	}
-
-	if isFirstSatInBlock && h%HalvingInterval == 0 {
-		return Epic
-	}
-
-	if isFirstSatInBlock && h%DificultyAdjustmentInterval == 0 {
-		return Rare
-	}
-
-	if isFirstSatInBlock {
-		return Uncommon
-	}
-
-	return Common
+func (s *Sat) IncrementTransactionCount() {
+    s.TransactionCount++
 }
 
-func (s Sat) IsBlack() bool {
-	return Sat(s + 1).IsFirstSatInBlock()
+func ProcessTransaction(tx Transaction) {
+    for _, output := range tx.Outputs {
+        // Предполагается, что вы можете получить объект Sat из output
+        satoshi := GetSatoshiFromOutput(output)
+        satoshi.IncrementTransactionCount()
+        // Обновите сатоши в вашей базе данных или кэше
+    }
 }
 
-func (s Sat) IsAlpha() bool {
-	return s%1e8 == 0
-}
-
-func (s Sat) IsOmega() bool {
-	return Sat(s + 1).IsAlpha()
-}
-
-func (s Sat) IsFibonacci() bool {
-	a := int64(0)
-	b := int64(1)
-	next := a + b
-	for next < int64(s) {
-		next = a + b
-		a = b
-		b = next
-	}
-
-	if s == 0 || s == 1 {
-		return true
-	}
-
-	return int64(s) == next
-}
-
-func (s Sat) Satributes() []Satribute {
-	exotics := make([]Satribute, 0)
-
-	rarity := s.GetRodarmorRarity()
-	if rarity != Common {
-		exotics = append(exotics, rarity)
-	}
-
-	if s.IsBlack() {
-		exotics = append(exotics, Black)
-	}
-
-	if s.IsAlpha() {
-		exotics = append(exotics, Alpha)
-	}
-
-	if s.IsOmega() {
-		exotics = append(exotics, Omega)
-	}
-
-	if s.IsFibonacci() {
-		exotics = append(exotics, Fibonacci)
-	}
-
-	if IsSatInRange(PizzaRanges, s) {
-		exotics = append(exotics, Pizza)
-	}
-
-	if IsSatInRange(FirstTransactionRanges, s) {
-		exotics = append(exotics, FirstTransaction)
-	}
-
-	if IsSatInRange(HitmanRanges, s) {
-		exotics = append(exotics, Hitman)
-	}
-
-	if IsSatInRange(JpegRanges, s) {
-		exotics = append(exotics, Jpeg)
-	}
-
-	h := s.Height()
-	if h == 9 {
-		exotics = append(exotics, Block9)
-	}
-
-	if h == 78 {
-		exotics = append(exotics, Block78)
-	}
-
-	if h <= 1000 {
-		exotics = append(exotics, Vintage)
-	}
-
-	if IsInBlocks(NakamotoBlocks, h) {
-		exotics = append(exotics, Nakamoto)
-	}
-
-	return exotics
-}
-
-func (s *Sat) AddTransactionID(txID string) {
-    s.TransactionIDs = append(s.TransactionIDs, txID)
-}
